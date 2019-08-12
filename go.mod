6e2b4831eee684fd0b7bb2d269bcb0bbdcce4bea
joy <joy.lai@zg.com>
2019-08-12 10:31:27 +0800

    fix

--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module github.com/pefish/go-logger
 
 require (
 	github.com/pefish/go-application v0.1.0
-	github.com/pefish/go-reflect v0.0.3
+	github.com/pefish/go-reflect v0.0.6
 	github.com/pefish/log4go v1.0.7
 	github.com/sirupsen/logrus v1.3.0
 	github.com/toolkits/file v0.0.0-20160325033739-a5b3c5147e07 // indirect
--- logger_test.go
+++ logger_test.go
@@ -1,11 +1,22 @@
 package go_logger
 
 import (
+	"errors"
+	"fmt"
 	"testing"
 )
 
 func TestLoggerClass_Debug(t *testing.T) {
+	defer func() {
+		if err := recover(); err != nil {
+			Logger.Error(err)
+		}
+	}()
+
 	loggerInstance := Log4goClass{}
 	Logger.Init(&loggerInstance, `test`)
 	Logger.Debug(1.344, `62562`)
+	Logger.Error(errors.New(`111`))
+	var a interface{} = `11`
+	fmt.Println(a.(int))
 }
